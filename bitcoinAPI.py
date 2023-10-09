67074aa558ebb2c8188579321f8886c3a7c26630
mahan ch <www.mahan.13831012c@gmail.com>
2023-10-09 17:26:34 +0330

    the project is finished (V1)

--- bitcoinAPI.py
+++ bitcoinAPI.py
@@ -1,5 +1,12 @@
+
 import requests
+my_money = 4500
 api = 'a583af9e9d6a8f76'
 response = requests.get('https://coinlib.io/api/v1/global?key=%s&pref=USD'% api)
 
-print(response.json().["coins"])
+coin_price= int(response.json()["coins"])
+# you can use another API to send you a SMS when ever the price was cheap enough
+if coin_price <= my_money:
+    print ('hey the BTC is cheap! lets buy it.')
+else:
+    print('not yet!')
